0389af8e30b28b4ca2a016f8fefac3f85ebd53c8
soniawan <soniinozz9@gmail.com>
2026-05-10 13:53:06 +0700

    feat: update app.js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-import "dotenv/config";
 import express from "express";
+import "dotenv/config";
+import { errorMiddleware } from "./utils/ErrorHandler.js";
 import productRouter from "./routes/productRouter.js";
+
 const app = express();
 
-app.use("/products", productRouter);
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use("/api/products", productRouter);
+
+// error middleware
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, (error) => {
-  if (error) {
-    throw error;
-  }
+app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
