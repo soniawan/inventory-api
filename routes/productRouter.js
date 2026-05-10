6269093e79d2f17f2604c21005d66b5fb4491e29
soniawan <soniinozz9@gmail.com>
2026-05-10 13:51:32 +0700

    feat(product): add product router

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,8 +1,31 @@
 import { Router } from "express";
-import { getAllProducts } from "../controllers/productController.js";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+} from "../controllers/productController.js";
+import {
+  validateCreateProduct,
+  validateProductId,
+  validateUpdateProduct,
+} from "../middleware/validators/productValidator.js";
+import validate from "../middleware/validate.js";
 
 const productRouter = Router();
 
 productRouter.get("/", getAllProducts);
+productRouter.post("/", validateCreateProduct, validate, createProduct);
+
+productRouter.get("/:id", validateProductId, validate, getProductById);
+productRouter.patch(
+  "/:id",
+  validateProductId,
+  validateUpdateProduct,
+  validate,
+  updateProduct,
+);
+productRouter.delete("/:id", validateProductId, validate, deleteProduct);
 
 export default productRouter;
